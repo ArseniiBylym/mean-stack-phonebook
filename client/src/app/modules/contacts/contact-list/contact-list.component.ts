6801a7121fbd453edbd5bf006c311a1cbe587d4c
Arsenii Bylym <arseniibylym@gmail.com>
2019-06-28 11:49:39 +0300

    Add dynamic searching

--- client/src/app/modules/contacts/contact-list/contact-list.component.ts
+++ client/src/app/modules/contacts/contact-list/contact-list.component.ts
@@ -10,20 +10,35 @@ import { ContactItem } from './../../../models/ContactItem.model';
 })
 export class ContactListComponent implements OnInit {
     contacts: ContactItem[];
+    filteredContacts: ContactItem[];
     contactsSub: Subscription;
+    searchString: string = '';
 
     constructor(
         private contactsService: ContactsService
     ) {}
-    
 
+    searchFilter(list: ContactItem[]) {
+        if (!this.searchString) return list;
+        return list.filter(item => {
+            const nameFound = item.name.toLowerCase().indexOf(this.searchString.toLocaleLowerCase()) > -1;
+            return nameFound;
+        })
+    }
+
+    onSearchChange() {
+        this.filteredContacts = this.searchFilter(this.contacts);
+    }
+    
     ngOnInit() {
         this.contactsService.selectedContact = null;
         this.contacts = this.contactsService.contacts;
+        this.filteredContacts = this.contactsService.contacts;
         this.contactsSub = this.contactsService.contactsUpdates
             .subscribe(
                 (contacts: ContactItem[]) => {
                     this.contacts = contacts;
+                    this.filteredContacts = this.searchFilter(contacts);
                 }
             )
         this.contactsService.getContacts();
--- client/src/app/modules/contacts/contacts.module.ts
+++ client/src/app/modules/contacts/contacts.module.ts
@@ -1,40 +1,41 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {AngularFireModule} from '@angular/fire';
-import {AngularFireStorageModule, StorageBucket } from '@angular/fire/storage';
+import {AngularFireStorageModule, StorageBucket} from '@angular/fire/storage';
 import {HttpClientModule} from '@angular/common/http';
 
-import { ContactsComponent } from './contacts.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
-import { ContactDetailsComponent } from './contact-details/contact-details.component';
-import { ContactCreateComponent } from './contact-create/contact-create.component';
-import { ContactEditComponent } from './contact-edit/contact-edit.component';
-import { ContactsRoutingModule } from './contacts-routing.module';
-import { ContactCreateService } from './../../services/contact-create.service';
-import { ContactsService } from './../../services/contacts.service';
-import { environment } from 'src/environments/environment';
+import {ContactsComponent} from './contacts.component';
+import {ContactListComponent} from './contact-list/contact-list.component';
+import {ContactDetailsComponent} from './contact-details/contact-details.component';
+import {ContactCreateComponent} from './contact-create/contact-create.component';
+import {ContactEditComponent} from './contact-edit/contact-edit.component';
+import {ContactsRoutingModule} from './contacts-routing.module';
+import {ContactCreateService} from './../../services/contact-create.service';
+import {ContactsService} from './../../services/contacts.service';
+import {environment} from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    ContactsComponent,
-    ContactListComponent, 
-    ContactDetailsComponent, 
-    ContactCreateComponent, 
-    ContactEditComponent
-  ],
-  imports: [
-  CommonModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    ContactsRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireStorageModule,
-  ],
-  providers: [
-    { provide: StorageBucket, useValue: 'ng-phonebook-58f04.appspot.com' },
-    ContactCreateService,
-    ContactsService,
-  ],
+    declarations: [
+        ContactsComponent,
+        ContactListComponent,
+        ContactDetailsComponent,
+        ContactCreateComponent,
+        ContactEditComponent,
+    ],
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule,
+        ContactsRoutingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireStorageModule,
+    ],
+    providers: [
+        {provide: StorageBucket, useValue: 'ng-phonebook-58f04.appspot.com'},
+        ContactCreateService,
+        ContactsService,
+    ],
 })
-export class ContactsModule { }
+export class ContactsModule {}
